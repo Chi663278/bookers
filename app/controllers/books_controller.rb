906342fc0c065be338aa4dfbdcdfabd37332ae90
Chi663278 <159125419+Chi663278@users.noreply.github.com>
2024-03-13 16:06:54 +0000

    [Update] 3-16 destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,17 @@
 class BooksController < ApplicationController
-  def new
-    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
-    @book = Book.new
-  end
-
   def create
     book = Book.new(book_params) #formから送られてくるデータはparamsの中に入っています
-    book.save #データをデータベースに保存するmethod
-    redirect_to book_path(book.id) #リダイレクト
+    if book.save #あとでやる
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      #あとでやる
+    end
   end
 
   def index #一覧画面用アクション
-    @books = Book.all #all method (モデルがやりとりしているデータベースのテーブルに保存されている、全てのレコードをまとめて取得)
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -31,7 +31,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    redirect_to books_path
   end
 
   private #ストロングパラメータbook_paramsを定義
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   root 'homes#top'
   resources :books #resources method
-  get 'books/:id' => 'books#show'
-
 end
